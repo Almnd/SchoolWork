5e1aad40ff3a13b3e2cd9bc82157052b0befcdee
Almnd <OliverAlmind@gmail.com>
2022-09-04 18:11:19 +0200

    Add files via upload

---
+++ raw12/WebService/Models/Profiles/FrameworkProfile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataServiceLib.Framework;
+using DataServiceLib.Moviedata;
+
+namespace WebService.Models.Profiles
+{
+    public class FrameworkProfile : Profile
+    {
+        public FrameworkProfile()
+        {
+            //FRAMEWORK
+
+            //usermap
+            CreateMap<Users, UsersDto>();
+            CreateMap<UserForCreationOrUpdateDto, Users>();
+            CreateMap<Users, UserElementDto>();
+
+            //searchmap
+            CreateMap<SearchHistory, SearchElementDto>();
+            CreateMap<SearchForCreationOrUpdateDto, StringSearchDto>();
+
+            //ratingmap
+            CreateMap<RatingHistory, RatingElementDto>();
+            CreateMap<RatingForCreationOrUpdateDto, RatingHistory>();
+
+            //tbooking map
+            CreateMap<TitleBookmarking, TBookElementDto>();
+
+            //abookingmap
+            CreateMap<ActorBookmarking, ABookElementDto>();
+
+            //stringsearchmap
+            CreateMap<SearchHistory, StringSearchDto>();
+            
+        }
+
+
+    }
+}
---
+++ raw12/WebService/Models/Profiles/MovieDataProfile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataServiceLib.Moviedata;
+
+namespace WebService.Models.Profiles
+{
+    public class MovieDataProfile : Profile
+    {
+        public MovieDataProfile()
+        {
+
+            //actor map
+            CreateMap<Actors, ActorDto>();
+
+            //movies map
+            CreateMap<Movies, MovieDto>();
+
+            //genre map
+            CreateMap<Genres, GenreDto>();
+
+            //details map
+            CreateMap<Details, DetailDto>();
+
+            //omdb map
+            CreateMap<Omdb, OmdbDto>();
+
+            //languages map
+            CreateMap<Languages, LanguageDto>();
+
+            //directors map
+            CreateMap<Directors, DirectorDto>();
+
+            //used to add from other tables to dto
+            /*CreateMap<Exampleclass, ExampleDto>()
+                .ForMember(src => src.Category,
+                    opt => opt.MapFrom(x => x.Category.Name));*/
+        }
+
+
+    }
+}
