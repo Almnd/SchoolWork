121879da5d46fb7bc54190fc1185f7e9a159f7da
Almnd <OliverAlmind@gmail.com>
2022-09-04 18:07:23 +0200

    Add files via upload

---
+++ raw12/DataServiceLib/Moviedata/Languages.cs
@@ -0,0 +1,12 @@
+﻿namespace DataServiceLib.Moviedata
+{
+    public class Languages
+    {
+        //properties
+        public string TitleId { get; set; }
+        public string Ordering { get; set; }
+        public string Region { get; set; }
+        public string Language { get; set; }
+
+    }
+}
